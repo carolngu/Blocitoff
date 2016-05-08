298eca97297ab1bf14253d8ce6a79a411c5f259e
Carol <carolngu@gmail.com>
2016-05-07 22:08:29 -0700

    6 Seeding Data: Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+    if @users.count > 1
+      grouped_users = @users.each_slice( (@users.count/2.0).round ).to_a
+      @first_grouped_users = grouped_users.first
+      @second_grouped_users = grouped_users.last
+    else
+      @first_grouped_users = @users
+    end
+  end
+
   def show
     @user = User.find_by(id: params[:id])
     if @user == current_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password(8)
+  )
+end
+users = User.all
+
+100.times do
+  Item.create!(
+  user: users.sample,
+  name: Faker::Lorem.word
+  )
+end
+items = Item.all
+
+puts "Seed finished"
+puts "#{User.count} users created."
+puts "#{Item.count} items created."
